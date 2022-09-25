3dab36fcc3595feb4707a1d203517256f2403903
Yoonbacca <yoonjaekim3@gmail.com>
2022-09-25 12:36:01 -0500

    Create playRound pseudocode

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
  //getComputerChoice randomly generates Rock, Paper, or Scissors
  function getComputerChoice() {
- //Declare variable 'choice' and assign value of 0-2
- let choice = Math.floor(Math.random()*3);
- //Create an switch statement for choice
- switch(choice)
+ let choice = Math.floor(Math.random()*3);  //Declare variable 'choice' and assign value of 0-2
+
+ switch(choice)  //Create an switch statement for choice
  {
      //Return 'Rock' for case 0
     case 0:
@@ -13,10 +12,20 @@
     case 1:
     return "Paper";
 
+     //Return 'Scissors' for case 2
     case 2:
     return "Scissors";
 
     default:
- //Return 'Scissors' for case 2
+
  }
 }
+
+//Declare function playRound with parameters playerSelection & computerSelection
+//Compare case-insensitive playerSelection & computerSelection using if/else
+//Win Condition: RvS || PvR || SvP
+//Return "You win! playerSelection beats computerSelection"
+//Lose Condition: RvP || PvS || SvR
+//Return "You lose! computerSelection beats playerSelection"
+//Tie Condition: computerSelection == playerSelection
+//Return "Tie! computerSelection is the same playerSelection"
