49bb1f7ef6feee5a7ea8e40e7d8d93004d6426f1
Yoonbacca <yoonjaekim3@gmail.com>
2022-09-25 14:15:16 -0500

    Create playRound

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
  //getComputerChoice randomly generates Rock, Paper, or Scissors
  function getComputerChoice() {
- let choice = Math.floor(Math.random()*3);  //Declare variable 'choice' and assign value of 0-2
+    //Declare variable 'choice' and assign value of 0-2
+    let choice = Math.floor(Math.random()*3);
 
- switch(choice)  //Create an switch statement for choice
- {
-     //Return 'Rock' for case 0
-    case 0:
-    return "Rock";
+    //Create an switch statement for choice
+    switch(choice)  
+    {
+        //Return 'Rock' for case 0
+        case 0:
+        return "Rock";
 
-    //Return 'Paper' for case 1
-    case 1:
-    return "Paper";
+        //Return 'Paper' for case 1
+        case 1:
+        return "Paper";
 
-     //Return 'Scissors' for case 2
-    case 2:
-    return "Scissors";
+        //Return 'Scissors' for case 2
+        case 2:
+        return "Scissors";
 
-    default:
+        default:
 
- }
+    }
 }
 
 //Declare function playRound with parameters playerSelection & computerSelection
-//Compare case-insensitive playerSelection & computerSelection using if/else
-//Win Condition: RvS || PvR || SvP
-//Return "You win! playerSelection beats computerSelection"
-//Lose Condition: RvP || PvS || SvR
-//Return "You lose! computerSelection beats playerSelection"
-//Tie Condition: computerSelection == playerSelection
-//Return "Tie! computerSelection is the same playerSelection"
+function playRound(playerSelection, computerSelection) {
+    //Compare case-insensitive playerSelection & computerSelection using if/else
+    const a = playerSelection.toUpperCase();
+    const b = computerSelection.toUpperCase();
+
+    //Win Condition: RvS || PvR || SvP
+    if ((a == "ROCK" && b == "SCISSORS") || (a == "PAPER" && b == "ROCK") || (a == "SCISSORS" && b == "PAPER")) {
+        //Return "You win! playerSelection beats computerSelection"
+        return `You win! ${a} beats ${b}`;
+
+    //Lose Condition: RvP || PvS || SvR
+    } else if ((a == "ROCK" && b == "PAPER") || (a == "PAPER" && b == "SCISSORS") || (a == "SCISSORS" && b == "ROCK")) {
+        //Return "You lose! computerSelection beats playerSelection"
+        return `You lose! ${b} beats ${a}`;
+
+    //Tie Condition: computerSelection == playerSelection
+    } else if (a == b) {
+        //Return "Tie! computerSelection is the same playerSelection"
+        return `Tie Game! You both chose ${a}`;
+
+    //Invalid Condition: Anything else that could occur would be invalid
+    } else {
+        return "Invalid entry! Try again!";
+    }
+
+
+}
+
+const playerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
