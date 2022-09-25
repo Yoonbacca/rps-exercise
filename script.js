45a9fdf63fe3e43b3e2f09aaa240ac29cd5b7e1f
Yoonbacca <yoonjaekim3@gmail.com>
2022-09-25 12:19:41 -0500

    Create pseudocode for getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+ //getComputerChoice randomly generates Rock, Paper, or Scissors
+ //Declare variable 'choice'
+ //Generate random integer value of 0, 1, or 2
+ //Create an switch statement for 0, 1, and 2
+ //Return 'Rock' for case 0
+ //Return 'Paper' for case 1
+ //Return 'Scissors' for case 2
