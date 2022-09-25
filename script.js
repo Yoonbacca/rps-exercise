2bae6e96c992222a1253da5b2a353d4607e7f1b4
Yoonbacca <yoonjaekim3@gmail.com>
2022-09-25 12:26:09 -0500

    Create getComputerChoice

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
  //getComputerChoice randomly generates Rock, Paper, or Scissors
- //Declare variable 'choice'
- //Generate random integer value of 0, 1, or 2
- //Create an switch statement for 0, 1, and 2
- //Return 'Rock' for case 0
- //Return 'Paper' for case 1
+ function getComputerChoice() {
+ //Declare variable 'choice' and assign value of 0-2
+ let choice = Math.floor(Math.random()*3);
+ //Create an switch statement for choice
+ switch(choice)
+ {
+     //Return 'Rock' for case 0
+    case 0:
+    return "Rock";
+
+    //Return 'Paper' for case 1
+    case 1:
+    return "Paper";
+
+    case 2:
+    return "Scissors";
+
+    default:
  //Return 'Scissors' for case 2
+ }
+}
